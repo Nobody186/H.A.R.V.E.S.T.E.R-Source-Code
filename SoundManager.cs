bcc47d5e3aa798db3f5c3bf51ae99d56a9e91aa0
Adham Abdelrhman <40753625+Nobody186@users.noreply.github.com>
2026-02-17 18:04:09 -0800

    SoundManager Refactor
    Refactored SoundManager to increase stability, handle ALL music, and allow for ease of use from other scripts through the Request() and EndRequest() functions.

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,56 +1,193 @@
-using System.Net;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] ConsoleController console;
-    [SerializeField] AudioSource ambience1;
-    [SerializeField] AudioSource ambience2;
-    [SerializeField] AudioSource finalHour;
 
-    bool alreadyPlayed;
+    public Stack<int> requests = new Stack<int>();
+    [SerializeField] List<Song> songs;
+    public Song currentSong;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    bool isTransitioning = false;
 
-    }
+    //Priority:
+    //5: Plot Event
+    //4: Final Hour
+    //3: Redactor
+    //2: Dogfight
+    //1: Ambience
+
+    //IDs:
+    //0 - Final Hour
+    //1 - Ambience1
+    //2 - Ambience2
+    //3 - Dogfight
+    //4 - Redactor
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeScale == 1 && !ambience1.isPlaying && !console.deathTime && !console.navMode && (console.currentWaypoint == 0 || console.currentWaypoint == 1))
+        CheckPause();
+        keepMusicPlaying();
+        ambienceRequest();
+        handleRequests();
+    }
+
+    void CheckPause()
+    {
+        if (Time.timeScale != 1 && currentSong.clip != null)
         {
-            ambience1.Play();
-            ambience2.Stop();
+            currentSong.clip.Pause();
+            return;
         }
-        else if(Time.timeScale == 1 && !ambience2.isPlaying && !console.deathTime && !console.navMode && (console.currentWaypoint == 2 || console.currentWaypoint == 4))
+        else if(Time.timeScale == 1 && currentSong.clip != null)
         {
-            ambience1.Stop();
-            ambience2.Play();
+            currentSong.clip.UnPause();
         }
-        else if(Time.timeScale == 1 && !finalHour.isPlaying && console.deathTime && !alreadyPlayed)
+    }
+
+    void keepMusicPlaying()
+    {
+        if(isTransitioning) return;
+        if (currentSong.clip != null)
         {
-            alreadyPlayed = true;
-            ambience1.Stop();
-            ambience2.Stop();
-            finalHour.Play();
-        }
-        if(console.isWarping || console.beingAttacked)
-        {
-            ambience1.Pause();
-            ambience2.Pause();
-        }
-        else if(!console.isWarping && !console.beingAttacked && !console.deathTime)
-        {
-            if ((console.currentWaypoint == 0 || console.currentWaypoint == 1 || console.currentWaypoint == 3) && !ambience1.isPlaying)
+            currentSong.clip.volume = 1;
+            if (!currentSong.clip.isPlaying)
             {
-                ambience1.UnPause();
-            }
-            else if((console.currentWaypoint == 2 || console.currentWaypoint == 4) && !ambience2.isPlaying)
-            {
-                ambience2.UnPause();
+                currentSong.clip.Play();
             }
         }
+    }
+
+    public void Request(int id) //I'll just keep this little song request bin.
+    {
+        requests.Push(id);
+    }
+
+    public void EndRequest(int id) //If an exit condition happens for a high priority song, we'll go through with it.
+    {
+        Song requestedSongEnd = songs[id];
+        if (currentSong.clip != null && requestedSongEnd.clip == currentSong.clip)
+        {
+            StartCoroutine(fadeOut());
+        }
+    }
+
+    void handleRequests() //Our fans will tell us to play their favorite songs because we are their favorite DJ. But not all songs are created equal. I have to choose which one to play, and make sure it doesn't loop every frame.
+    {
+        print("REQUESTS: " + requests);
+        Song highestPrioritySong = new Song();
+        while(requests.Count > 0)
+        {
+            int id = requests.Pop();
+            Song requestedSong = songs[id];
+            if(currentSong.clip == null)
+            {
+                currentSong.clip = requestedSong.clip; //Normally this would cause problems (current song plays no matter what and we're just playing a random song here). But we switch everything around in the same frame anyway, the player probably wont notice.
+                currentSong.priority = requestedSong.priority;
+            }
+            if(requestedSong.clip == currentSong.clip)
+            {
+                continue;
+            }
+            if(requestedSong.priority >= currentSong.priority && requestedSong.priority >= highestPrioritySong.priority) //If our currently selected track has higher priority than the current music and the last music we've pulled, select it.
+            {
+                highestPrioritySong = requestedSong;
+            }
+        }
+        if (highestPrioritySong.clip != null)
+        {
+            print("HIGHEST PRIORITY: " +  highestPrioritySong.clip.name);
+            StartCoroutine(transition(highestPrioritySong));
+        }
+    }
+
+    void ambienceRequest()
+    {
+        if (console.canWarp && !console.navMode) //Make sure we're not changing waypoints.
+        {
+            //Waypoints: 0 - Initial Point, 1 - Cluster Field, 2 - Sancturary, 3 - Greatness, 4 - Dead Man's Zone
+            switch (console.currentWaypoint)
+            {
+                case 1:
+                    Request(1);
+                    break;
+                case 2:
+                    Request(2);
+                    break;
+                case 3:
+                    Request(1);
+                    break;
+                case 4:
+                    Request(2);
+                    break;
+            }
+        }
+    }
+
+    IEnumerator transition(Song newSong) //This little trick is why I'm everyone's favorite DJ.
+    {
+        while(isTransitioning)
+        {
+            print("TRANSITION IS AWAITING OTHER TRANSITION");
+            yield return null;
+        }
+        isTransitioning = true;
+        if (currentSong.clip != null && currentSong.clip.volume != 0) //If we have a song already playing, fade it out.
+        {
+            while (currentSong.clip.volume > 0)
+            {
+                currentSong.clip.volume -= Time.deltaTime * 3f;
+                yield return new WaitForEndOfFrame();
+            }
+            print("TRANSITIONER IS FADING OUT");
+        }
+
+        currentSong.clip = newSong.clip;
+        currentSong.priority = newSong.priority;
+        currentSong.clip.Play();
+
+        while (currentSong.clip.volume < 1) //Bring in the new music.
+        {
+            currentSong.clip.volume += Time.deltaTime*3f;
+            yield return new WaitForEndOfFrame();
+        }
+        print("TRANSITIONER HAS COME BACK!");
+        isTransitioning = false;
+    }
+
+    IEnumerator fadeOut()
+    {
+        while(isTransitioning)
+        {
+            print("FADING OUT, AWAITING TRANSITION.");
+            yield return null;
+        }
+        print("FADE OUT COMMENCED");
+        isTransitioning = true;
+        if (currentSong.clip != null && currentSong.clip.volume != 0) //If we got music, fade it out.
+        {
+            while (currentSong.clip.volume > 0)
+            {
+                currentSong.clip.volume -= Time.deltaTime * 3f;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        print("SONG NO LONGER PLAYING");
+        currentSong.clip = null;
+        currentSong.priority = 0;
+        isTransitioning = false;
+    }
+
+    [System.Serializable]
+    public class Song
+    {
+        public AudioSource clip = null;
+        public int priority = 0;
     }
 }
