31f644c4d1933903cb32893e75c79973ab961d52
Adham Abdelrhman <40753625+Nobody186@users.noreply.github.com>
2026-03-08 22:17:50 -0700

    More control over game and other triggers. Also added interrupt system

--- storyTrigger.cs
+++ storyTrigger.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class storyTrigger : MonoBehaviour
 {
+    [SerializeField] ConsoleController consoleController;
     [SerializeField] GameObject dayIntro;
     [SerializeField] StoryManager storyManager;
     [SerializeField] string keybindMessage;
@@ -18,13 +20,22 @@ public class storyTrigger : MonoBehaviour
     [SerializeField] List<string> caps;
     [SerializeField] List<float> times;
 
+    [SerializeField] List<GameObject> triggersToDestroy;
+    [SerializeField] List<GameObject> triggersToEnable;
+    [SerializeField] List<int> daysICanLive;
+
     [SerializeField] bool freezePlayer;
+    [SerializeField] bool interrupt;
     float timer = 0f;
     float clipTime = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         messageToPlay = GetComponent<AudioSource>();
+        if(!daysICanLive.Contains(consoleController.day))
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -32,7 +43,30 @@ public class storyTrigger : MonoBehaviour
         if (!alreadyTriggered && other.gameObject.name == "PlayerShip" && !dayIntro.activeSelf)
         {
             alreadyTriggered = true;
-            StartCoroutine(storyManager.playNextStep(messageToPlay, showKeybindMessage, keybindMessage, showObjectiveMessage, objectiveName, keybind, longKeybindName, caps, times, freezePlayer));
+
+            StoryMessage message = new StoryMessage();
+            message.subtitles = caps;
+            message.timestamps = times;
+            message.audio = messageToPlay;
+            message.showControlText = showKeybindMessage;
+            message.controlText = keybindMessage;
+            message.bind = keybind;
+            message.customInstruction = longKeybindName;
+            message.freezePlayer = freezePlayer;
+            message.whiteOut = false;
+            message.interrupt = interrupt;
+
+            storyManager.EnqueueMessage(message);
+
+            for(int i = 0; i < triggersToDestroy.Count; i++)
+            {
+                Destroy(triggersToDestroy[i]);
+            }
+            for(int i = 0; i < triggersToEnable.Count; i++)
+            {
+                triggersToEnable[i].SetActive(true);
+            }
+
             print("TRIGGER FIRED!");
             clipTime = messageToPlay.clip.length;
         }
